9ab8c4f31fa7dfc596cbb4e404a011ac65df20b1
Muhammad Amien Sidiq <44141758+muhamien12@users.noreply.github.com>
2019-05-16 00:02:25 +0700

    Add files via upload

---
+++ Latihan 3/latihan3.py
@@ -0,0 +1,66 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class MainForm(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setupUi()
+
+
+	def setupUi(self):
+		self.resize(500, 300)
+		self.move(300, 300)
+		self.setWindowTitle('Penggunaan QTextEdit')
+
+		self.label1 = QLabel()
+		self.label1.setText('Dari')
+		self.phoneEdit1 = QLineEdit()
+
+		self.label3 = QLabel()
+		self.label3.setText('Untuk<b>')
+		self.phoneEdit = QLineEdit()
+
+		vbox1 = QVBoxLayout()
+		vbox1.addWidget(self.label1)
+		vbox1.addWidget(self.phoneEdit)
+		vbox1.addWidget(self.label3)
+		vbox1.addWidget(self.phoneEdit1)
+
+		self.label2 = QLabel()
+		self.label2.setText(' ')
+		self.messageEdit = QTextEdit()
+
+		vbox2 = QVBoxLayout()
+		vbox2.addWidget(self.label2)
+		vbox2.addWidget(self.messageEdit)
+
+		vbox3 = QVBoxLayout()
+		vbox3.addLayout(vbox1)
+		vbox3.addLayout(vbox2)
+
+		self.sendButton = QPushButton('Kirim SMS')
+		self.cancelButton = QPushButton('&Batal')
+		hbox = QHBoxLayout()
+		hbox.addStretch()
+		hbox.addWidget(self.sendButton)
+		hbox.addWidget(self.cancelButton)
+
+		layout = QVBoxLayout()
+		layout.addLayout(vbox3)
+
+		horizontalLine = QFrame();
+		horizontalLine.setFrameShape(QFrame.HLine)
+		horizontalLine.setFrameShadow(QFrame.Sunken)
+		layout.addWidget(horizontalLine)
+		layout.addLayout(hbox)
+		self.setLayout(layout)
+
+if __name__ == '__main__':
+	a = QApplication(sys.argv)
+	form = MainForm()
+	form.show()
+
+	a.exec_() 
